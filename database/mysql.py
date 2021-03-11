18b7b27a2e5608e6195f8ea22ac4875c0209adc8
volleyball26 <sunliren@mshare.cn>
2021-03-11 11:34:40 +0800

    exex_safe

--- database/mysql.py
+++ database/mysql.py
@@ -3,8 +3,6 @@ from __future__ import print_function
 import logging
 
 from defines.config import *
-from tornado.gen import coroutine
-from tornado.ioloop import IOLoop
 from tornado_mysql import pools
 
 
@@ -27,32 +25,46 @@ class Mysql:
     def __error_log(self, sqlstr, parm=None, error=None):
         logging.error('[sqlerror]sql is: {0}\nparms is:{1}\nerror is:{2}'.format(sqlstr, parm, error))
 
-    def __format_sql(self, sqlstr, parm=None):
+    def __format_sql(self, sql_str, parm=None):
         _parm = []
         if parm is not None:
             for p in parm:
                 _parm.append("'%s'" % p)
-            outstr = sqlstr % tuple(_parm)
+            out_str = sql_str % tuple(_parm)
         else:
-            outstr = sqlstr
-        return outstr
+            out_str = sql_str
+        return out_str
 
-    def __print_sql(self, sqlstr, parm=None):
+    def __print_sql(self, sql_str, parm=None):
         """打印完整的sql语句，方便调试"""
 
-        outstr = self.__format_sql(sqlstr, parm)
-        logging.debug(outstr)
-        # print('{}'.format(outstr))
+        out_str = self.__format_sql(sql_str, parm)
+        logging.debug(sql_str)
 
-    async def query(self, sqlstr):
+    async def query(self, sql_str):
         result = []
         try:
-            self.__print_sql(sqlstr)
-            cursor = await self.pool.execute(sqlstr)
+            self.__print_sql(sql_str)
+            cursor = await self.pool.execute(sql_str)
             _fetch = cursor.fetchall()
             if _fetch:
                 result = _fetch
         except Exception as e:
-            self.__error_log(sqlstr, (), repr(e))
+            self.__error_log(sql_str, (), repr(e))
 
         return result
+
+    async def exec_safe(self, sql_str: str, args) -> bool:
+        result = True
+
+        try:
+            self.__print_sql(sql_str, args)
+            cursor = await self.pool.execute(sql_str, args)
+            return cursor.rowcount
+
+        except Exception as e:
+            self.__error_log(sql_str, args, repr(e))
+            result = False
+
+        finally:
+            return result
